57bfb7b1d00f30666f08f8a56e7edb9977275c34
Alexis Tabin <43467095+AlexisTabin@users.noreply.github.com>
2024-11-03 20:11:25 +0100

    Time: 3 ms (95.79%), Space: 16.8 MB (56.30%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            iso_map = {}
-            for i, c in enumerate(s):
-                if c in iso_map:
-                    if iso_map[c] != t[i]:
-                        return False
-                else:
-                    if t[i] in iso_map.values():
-                        return False
-                    iso_map[c] = t[i]
-            return True
-        else:
-            return False
+        iso_map = {}
+        for i, c in enumerate(s):
+            if c in iso_map:
+                if iso_map[c] != t[i]:
+                    return False
+            else:
+                if t[i] in iso_map.values():
+                    return False
+                iso_map[c] = t[i]
+        return True
         
